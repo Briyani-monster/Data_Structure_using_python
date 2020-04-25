95544acd35abf43691f57ffc17845bbba908e45d
gsAshish <gsashish69@gmail.com>
2020-04-25 13:47:00 +0530

    general update in exercise

--- exercise reverse string.py
+++ exercise reverse string.py
@@ -8,6 +8,21 @@ def reverse(stri):
     back.append(li[i-1])
     i-=1
   return "".join(back)
+
+#using build in function  
+def reverse1(stri):
+  if len(str(stri))<2 or not type(stri)==str:
+    return 'hmm something is wrong here'
+  li=list(stri)
+  li.reverse()
+  return "".join(li)  
+
 print(reverse('hello i am kishan'))
 print(reverse(1234))
-print(reverse(''))    
+print(reverse('hello my name is andrei'))  
+print("second function")
+print(reverse1('hello i am kishan'))
+print(reverse1(1234))
+print(reverse1('hello my name is andrei'))  
+
+
